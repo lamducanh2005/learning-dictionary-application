7264c6a6dbe19b28ce6ee53e4687d2dec9ff5bc4
lamducanh2005 <23020326@vnu.edu.vn>
2024-04-05 23:18:41 +0700

    change the style of navigation

--- frontend/components/LambdaComponents/LambdaNavigation/LambdaNavigation.tsx
+++ frontend/components/LambdaComponents/LambdaNavigation/LambdaNavigation.tsx
@@ -7,8 +7,23 @@ export default function LambdaNavigation(props : any) {
             onClose={props.onClose}
             placement={"left"}
         >
-            <DrawerOverlay />
-            <DrawerContent>
+            <DrawerOverlay
+                style={{
+                    backgroundColor: 'rgba(200, 200, 200, 0.25)',
+                    backdropFilter: 'blur(5px)',
+                }}
+            />
+            <DrawerContent
+                style={{
+                    backgroundColor: '#ffffff',
+                    top: 10,
+                    left: 10,
+                    height: 'calc(100% - 20px)',
+                    borderRadius: '10px',
+                    border: '1px solid #eee',
+                    backdropFilter: 'blur(5px)',
+                }}
+            >
                 <DrawerCloseButton />
                 <DrawerHeader>Lambda</DrawerHeader>
             </DrawerContent>
--- frontend/components/LambdaComponents/LambdaNavigation/LambdaNavigationButton.tsx
+++ frontend/components/LambdaComponents/LambdaNavigation/LambdaNavigationButton.tsx
@@ -19,6 +19,7 @@ export default function LambdaNavigationButton() {
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
+                cursor: 'pointer',
             }}
             onClick={onOpen}
         >
