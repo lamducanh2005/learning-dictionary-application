c3adc3dcee88f1093d9b26179d496aab3f9e2a1a
lamducanh2005 <23020326@vnu.edu.vn>
2024-04-22 09:53:51 +0700

    delete word feature in collection view

--- frontend/components/Collection/CollectionBody/CollectionList/CollectionPanel/PanelWordList.tsx
+++ frontend/components/Collection/CollectionBody/CollectionList/CollectionPanel/PanelWordList.tsx
@@ -1,20 +1,28 @@
 import 'Frontend/themes/Collection/CollectionList/CollectionPanel/PanelWordList.css';
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {CollectionContext} from "Frontend/components/Collection/CollectionBody";
 import Word from "Frontend/generated/com/application/models/Word";
 import PanelWordListItem
     from "Frontend/components/Collection/CollectionBody/CollectionList/CollectionPanel/PanelWordListItem";
+import {CollectionService} from "Frontend/generated/endpoints";
 
 export default function PanelWordList() {
 
     const collection = useContext(CollectionContext);
+    const [wordList, setWordList] = useState(collection.words);
 
+    const handleDelete = async (wordId: number) => {
+        if (collection.id) {
+            await CollectionService.removeWordFromCollection(wordId, collection.id);
+            setWordList(await CollectionService.getAllWordsByCollectionId(collection.id))
+        }
+    }
 
     return (
         <div className={"panel-word-list"}>
             {   //@ts-ignore
-                collection.words.map((word: Word) => {
-                    return <PanelWordListItem key={word.id} word={word}/>
+                wordList.map((word: Word) => {
+                    return <PanelWordListItem key={word.id} word={word} onDelete={handleDelete}/>
                 })
             }
         </div>
--- frontend/components/Collection/CollectionBody/CollectionList/CollectionPanel/PanelWordListItem.tsx
+++ frontend/components/Collection/CollectionBody/CollectionList/CollectionPanel/PanelWordListItem.tsx
@@ -1,7 +1,72 @@
+import {DeleteIcon} from "@chakra-ui/icons";
+import {motion} from "framer-motion";
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogContent, AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogOverlay, Button,
+    useDisclosure
+} from "@chakra-ui/react";
+import {useRef} from "react";
+
 export default function PanelWordListItem(props: any) {
+
+    const {isOpen, onOpen, onClose} = useDisclosure()
+    const cancelRef = useRef<any>()
+
+    const handleDelete = () => {
+        props.onDelete(props.word.id);
+        onClose();
+    }
+
     return (
         <div className={"panel-word-list-item"}>
-            {props.word.word}
+            <span className={"word"}>{props.word.word}</span>
+            <motion.button
+                className={"delete-button"}
+                whileHover={{scale: 1.2}}
+                whileTap={{scale: 0.9}}
+                onClick={onOpen}
+            >
+                <DeleteIcon className={"icon"}/>
+            </motion.button>
+
+            <AlertDialog
+                leastDestructiveRef={cancelRef}
+                isOpen={isOpen}
+                onClose={onClose}
+                motionPreset={'slideInBottom'}
+                isCentered
+            >
+                <AlertDialogOverlay style={{
+                    backgroundColor: 'rgba(150, 150, 150, 0.25)'
+                }}/>
+                <AlertDialogContent className={"delete-alert-dialog"}>
+                    <AlertDialogHeader>Chắc chắn xóa?</AlertDialogHeader>
+                    <AlertDialogBody>
+                        Bạn có chắc chắn muốn xóa từ {props.word.word} ra khỏi bộ sưu tập này không?
+                    </AlertDialogBody>
+
+                    <AlertDialogFooter>
+                        <motion.button
+                            className={"cancel-button"}
+                            ref={cancelRef}
+                            onClick={onClose}
+                            whileHover={{scale: 1.1}}
+                            whileTap={{scale: 0.9}}
+                        >Không
+                        </motion.button>
+                        <motion.button
+                            className={"confirm-button"}
+                            onClick={handleDelete}
+                            whileHover={{scale: 1.1}}
+                            whileTap={{scale: 0.9}}
+                        >Chắn chắn xóa
+                        </motion.button>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
         </div>
     )
 }
